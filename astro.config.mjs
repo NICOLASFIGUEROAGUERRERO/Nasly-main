0ee62bad4b048b6e74e29d424031a76aa2341ddb
NICOLASFIGUEROAGUERRERO <184985218+NICOLASFIGUEROAGUERRERO@users.noreply.github.com>
2025-12-01 14:41:18 -0500

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,8 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   server: {
-    host: true,     
-    port: Number(process.env.PORT) || 8080
+    host: "0.0.0.0",              
+    port: Number(process.env.PORT) || 8080,
   },
 });
+
